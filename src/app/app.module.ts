f05c43f2bbe12e0f0e9639a9d224632b037c911c
Juliana Salazar <limajuliana115@yahoo.com.br>
2022-01-17 13:13:21 -0300

    Primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './views/shared/top-bar/top-bar.component';
+import { FeedComponent } from './views/pages/feed/feed.component';
+import { PostComponent } from './views/shared/post/post.component';
+import { CommentComponent } from './views/shared/comment/comment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    FeedComponent,
+    PostComponent,
+    CommentComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/views/pages/feed/feed.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.css']
+})
+export class FeedComponent implements OnInit {
+  posts = [
+    {
+      id: 40,
+      title: 'Testee',
+      description: 'teste, teste',
+      image: "https://media.bitdegree.org/storage/media/images/2018/12/node-js-interview-questions-logo-2-266x300.png",
+      gist: null,
+      created_at: '2021-11-10T18:04:09.000Z',
+      StudentId: 1,
+      Student: {
+        id: 1,
+        name: 'Rafael',
+        image: 'http://fiap-social-api.herokuapp.com/uploads/1636312523776.jpg',
+      },
+      Categories: [
+        {
+          id: 5,
+          description: 'HTML',
+        },
+        {
+          id:6,
+          description: 'CSS',
+        },
+      ],
+      Answers: [
+        {
+          id: 255,
+          description: 'testeee quantidade de caracteres',
+          created_at: '2021-11-10T18:13:15.000Z',
+          Student: {
+            id: 1,
+            name: 'Rafael',
+            image:
+              'http://fiap-social-api.herokuapp.com/uploads/1636312523776.jpg',
+          },
+        },
+        {
+          id: 256,
+          description: 'testando o comentário',
+          created_at: '2021-11-10T19:50:59.000Z',
+          Student: {
+            id: 1,
+            name: 'Rafael',
+            image:
+              'http://fiap-social-api.herokuapp.com/uploads/1636312523776.jpg',
+          },
+        },
+      ],
+    }
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
